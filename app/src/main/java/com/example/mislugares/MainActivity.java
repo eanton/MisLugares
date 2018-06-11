622462ac18ac3957b76d3db0664fb1c9a443833d
eanton <eanton@gmail.com>
2018-06-11 12:03:25 +0200

    Ejercicio 3 Android - Vistas y Layouts

--- app/src/main/java/com/example/mislugares/MainActivity.java
+++ app/src/main/java/com/example/mislugares/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
