598a1638f54e9f6645fe6783f6ee3c030e47190f
eanton <eanton@gmail.com>
2018-06-23 16:51:06 +0200

    Ejercicio 5 Android - Actividades (y eventos)

---
+++ app/src/main/java/com/example/mislugares/AcercaDeActivity.java
@@ -0,0 +1,14 @@
+package com.example.mislugares;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class AcercaDeActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
--- app/src/main/java/com/example/mislugares/MainActivity.java
+++ app/src/main/java/com/example/mislugares/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mislugares;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bAcercaDe;
+    private Button bSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,26 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        bAcercaDe = (Button) findViewById(R.id.button03);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarAcercaDe();
+            }
+        });
+
+        bSalir = (Button) findViewById(R.id.button04);
+        bSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+
 
     }
 
@@ -51,4 +76,11 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void lanzarAcercaDe(){
+        Intent i = new Intent(this, AcercaDeActivity.class);
+        startActivity(i);
+    }
+
+
 }
